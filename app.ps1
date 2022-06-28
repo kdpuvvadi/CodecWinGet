b9a419fca8318224cf43cbb12047501a7676fcbf
KD Puvvadi <kdpuvvadi@gmail.com>
2022-06-28 11:37:36 +0530

    Check for update

--- app.ps1
+++ app.ps1
@@ -7,11 +7,19 @@ $Channels = @(
     "Mega"
 )
 foreach ($Channel in $Channels) {
-    $GetWinGetVersion = (winget show "$PackageName.$Channel" | findstr -i Version).Substring(9)
+    $GetWinGetVersion = (WinGet show "$PackageName.$Channel" | findstr -i Version).Substring(9)
     Write-Output  "WinGet Version of $PackageName.$Channel is $GetWinGetVersion"
 
     $GetLatestVersion = ((Get-NevergreenApp -Name KLiteCodecPack | Where-Object {$_.Channel -eq $Channel}).Version)
     Write-Output "Latest Version of $PackageName.$Channel is $GetLatestVersion"
+
+    if ($GetWinGetVersion -lt $GetLatestVersion) {
+        Write-Output "Update available"
+    }
+    else {
+        Write-Output "You've latest version installed"
+    }
+    
     Write-Output ""
 }
  

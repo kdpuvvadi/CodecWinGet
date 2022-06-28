1796edaaeafd3b8f2a5822a97d8cb15e7926a22b
KD Puvvadi <kdpuvvadi@gmail.com>
2022-06-28 11:29:48 +0530

    Get version from original source

--- app.ps1
+++ app.ps1
@@ -10,6 +10,8 @@ foreach ($Channel in $Channels) {
     $GetWinGetVersion = (winget show "$PackageName.$Channel" | findstr -i Version).Substring(9)
     Write-Output  "WinGet Version of $PackageName.$Channel is $GetWinGetVersion"
 
-   
+    $GetLatestVersion = ((Get-NevergreenApp -Name KLiteCodecPack | Where-Object {$_.Channel -eq $Channel}).Version)
+    Write-Output "Latest Version of $PackageName.$Channel is $GetLatestVersion"
+    Write-Output ""
 }
  

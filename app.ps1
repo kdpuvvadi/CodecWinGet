92facdf52c22858de6bc6c55723b46a0be35c642
KD Puvvadi <kdpuvvadi@gmail.com>
2022-06-29 11:28:06 +0530

    messages

--- app.ps1
+++ app.ps1
@@ -17,14 +17,20 @@ $Channels = @(
     "Mega"
 )
 foreach ($Channel in $Channels) {
+    Write-Output "Retrieving WinGet version of $WinGetPackage.$Channel"
     $GetWinGetVersion = (WinGet show "$WinGetPackage.$Channel" | findstr -i Version).Substring(9)
-    # Write-Output  "WinGet Version of $WinGetPackage.$Channel is $GetWinGetVersion"
+    Write-Output  "WinGet Version of $WinGetPackage.$Channel is $GetWinGetVersion"
+    Write-Output ""
 
+    Write-Output "Retrieving Latest version of $WinGetPackage.$Channel"
     $GetLatestVersion = ((Get-NevergreenApp -Name $NevergreenPackage | Where-Object {$_.Channel -eq $Channel}).Version)
-    # Write-Output "Latest Version of $WinGetPackage.$Channel is $GetLatestVersion"
+    Write-Output "Latest Version of $WinGetPackage.$Channel is $GetLatestVersion"
+    Write-Output ""
 
+    Write-Output "Retrieving Latest version $GetLatestVersion URL"
     $GetLatestUri =  ((Get-NevergreenApp -Name $NevergreenPackage | Where-Object {$_.Channel -eq $Channel}).Uri)
-    # Write-Output $GetLatestUri
+    Write-Output $GetLatestUri
+    Write-Output ""
 
     if ($GetWinGetVersion -lt $GetLatestVersion) {
         Write-Output "Update available"

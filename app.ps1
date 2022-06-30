34a401fcb6d04603b9e15b95cde5d9e52c00ce50
KD Puvvadi <kdpuvvadi@gmail.com>
2022-06-30 20:30:47 +0530

    using Github API to get latest version

--- app.ps1
+++ app.ps1
@@ -17,13 +17,19 @@ $Channels = @(
     "Mega"
 )
 foreach ($Channel in $Channels) {
+    
     Write-Host -NoNewline "Retrieving WinGet version of $WinGetPackage.$Channel "
-    $GetWinGetVersion = (WinGet show "$WinGetPackage.$Channel" | findstr -i Version).Substring(9)
-    Write-Host "Done" -ForegroundColor Green
+    $WingetURL = "https://api.github.com/repos/microsoft/winget-pkgs/contents/manifests/c/CodecGuide/K-LiteCodecPack/$($Channel)?ref=master"
+    $APIResponse = ((Invoke-WebRequest  -uri $WingetURL).Content | ConvertFrom-Json)
+    $versions= (($APIResponse | Format-Table -Property Name | out-string).Trim())
+    $GetWinGetVersion = $versions.Substring($versions.Length-6)
+    Write-Host -NoNewline "Done " -ForegroundColor Green
+    Write-Host $GetWinGetVersion -ForegroundColor Red
 
     Write-Host -NoNewline "Retrieving Latest version of $WinGetPackage.$Channel "
     $GetLatestVersion = ((Get-NevergreenApp -Name $NevergreenPackage | Where-Object {$_.Channel -eq $Channel}).Version)
-    Write-Host "Done" -ForegroundColor Green
+    Write-Host -NoNewline "Done " -ForegroundColor Green
+    Write-Host $GetLatestVersion -ForegroundColor Red
 
     if ($GetWinGetVersion -lt $GetLatestVersion) {
         Write-Host -NoNewline "Retrieving Latest version $GetLatestVersion URL "
